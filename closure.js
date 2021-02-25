4c91b23a2cb64382b3b663cb4594b502e3d2dbf3
Jon Craton <jncraton@gmail.com>
2021-02-25 11:57:51 -0500

    Fix with standard

--- closure.js
+++ closure.js
@@ -2,53 +2,50 @@ const assert = require('assert')
 
 // Replace the following class with an object factory using a closure pattern
 
-// function Car(acceleration, top_speed) {
+// function Car(acceleration, topSpeed) {
 
 class Car {
-  constructor(acceleration, top_speed) {
+  constructor (acceleration, topSpeed) {
     this.acceleration = acceleration
-    this.top_speed = top_speed
+    this.topSpeed = topSpeed
     this.position = 0
     this.speed = 0
-
   }
 
-  get_position() {
+  getPosition () {
     return this.position
   }
 
-  update() {
+  update () {
     this.speed += this.acceleration
-    this.speed = Math.min(this.top_speed, this.speed)
+    this.speed = Math.min(this.topSpeed, this.speed)
     this.position += this.speed
   }
 }
 
-
-
-function race(cars) {
+function race (cars) {
   console.clear()
   cars.forEach((car, i) => {
     car.update()
-    const pos = car.get_position()
-    console.log(`${" ".repeat(pos)}${i}`)
+    const pos = car.getPosition()
+    console.log(`${' '.repeat(pos)}${i}`)
   })
 
-  if (cars.every(c => c.get_position() < 80)) {
+  if (cars.every(c => c.getPosition() < 80)) {
     setTimeout(() => race(cars), 300)
   }
 }
 
 // Tests
-const car = new Car(1,2)
-assert(car.get_position() == 0, "Position does not start at 0")
-assert(car.update() || car.get_position() == 1, "Position does not update correctly")
-assert(car.update() || car.get_position() == 3, "Position does not update correctly")
-assert(car.position == undefined, "Position is public")
-assert(car.speed == undefined, "Speed is public")
+const car = new Car(1, 2)
+assert(car.getPosition() === 0, 'Position does not start at 0')
+assert(car.update() || car.getPosition() === 1, 'Position does not update correctly')
+assert(car.update() || car.getPosition() === 3, 'Position does not update correctly')
+assert(car.position === undefined, 'Position is public')
+assert(car.speed === undefined, 'Speed is public')
 
 race([
-  new Car(1.2,6),
-  new Car(1,12),
-  new Car(1.1,8),
+  new Car(1.2, 6),
+  new Car(1, 12),
+  new Car(1.1, 8)
 ])
